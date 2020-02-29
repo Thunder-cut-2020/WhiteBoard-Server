d39c81a79c4b097931230b7ef9dd18194ea27ce6
Seokjin Yoon <sjy5386@gmail.com>
2020-02-29 21:50:10 +0900

    Add class Data

---
+++ src/com/thunder_cut/netio/Data.java
@@ -0,0 +1,46 @@
+/*
+ * Data.java
+ * Author: Seokjin Yoon
+ * Created Date: 2020-02-29
+ */
+
+package com.thunder_cut.netio;
+
+import com.thunder_cut.encryption.SymmetricKeyEncryption;
+
+import javax.crypto.SecretKey;
+import java.nio.ByteBuffer;
+
+public class Data {
+    public final DataType dataType;
+    public final int srcId;
+    private byte[] data;
+
+    public Data(DataType dataType, int srcId, byte[] data) {
+        this.dataType = dataType;
+        this.srcId = srcId;
+        this.data = data;
+    }
+
+    public Data(byte[] encryptedData, SecretKey secretKey) {
+        SymmetricKeyEncryption encryption = new SymmetricKeyEncryption(secretKey);
+        byte[] decrypted = encryption.decrypt(encryptedData);
+        ByteBuffer byteBuffer = ByteBuffer.wrap(decrypted);
+        dataType = DataType.valueOf(byteBuffer.getChar());
+        srcId = byteBuffer.getInt();
+        byteBuffer.get(data);
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public ByteBuffer toEncrypted(SecretKey secretKey) {
+        SymmetricKeyEncryption encryption = new SymmetricKeyEncryption(secretKey);
+        byte[] encrypted = encryption.encrypt(data);
+        ByteBuffer byteBuffer = ByteBuffer.allocate(Integer.BYTES + encrypted.length);
+        byteBuffer.putInt(encrypted.length);
+        byteBuffer.put(encrypted);
+        return byteBuffer;
+    }
+}
