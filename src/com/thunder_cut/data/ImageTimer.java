fe882133e1a8db3b37131c3281fd82fbb9b6a1b9
Seokjin Yoon <sjy5386@gmail.com>
2020-03-08 21:52:59 +0900

    Add ImageTimer

---
+++ src/com/thunder_cut/data/ImageTimer.java
@@ -0,0 +1,36 @@
+/*
+ * ImageTimer.java
+ * Author: Seokjin Yoon
+ * Created Date: 2020-03-08
+ */
+
+package com.thunder_cut.data;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ImageTimer {
+    private ScheduledExecutorService scheduledExecutorService;
+    private long fps;
+
+    public ImageTimer() {
+        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+    }
+
+    public void start(long fps) {
+        this.fps = fps;
+        scheduledExecutorService.scheduleAtFixedRate(this::run, 0, 1000L / fps, TimeUnit.MILLISECONDS);
+    }
+
+    public void stop() {
+        scheduledExecutorService.shutdown();
+    }
+
+    private void run() {
+    }
+
+    public long getFps() {
+        return fps;
+    }
+}
